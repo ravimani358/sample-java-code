8dd68655684e25436cfefb506e39032a8b7c1304
ravimani358 <Ravimani358@gmail.com>
2024-06-03 14:50:01 +0530

    Initial commit

---
+++ eclips_respace/sheet/src/main/java/sheet/read.java
@@ -0,0 +1,10 @@
+package sheet;
+
+public class read {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
---
+++ eclips_respace/sheet/src/main/java/sheet/right.java
@@ -0,0 +1,10 @@
+package sheet;
+
+public class right {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
